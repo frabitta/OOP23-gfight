612895c6611821ac4ebd36f062960ca3709478f1
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-01-03 16:44:39 +0100

    Created build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,27 @@
+plugins {
+    java
+    application
+    id("org.danilopianini.gradle-java-qa") version "1.32.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
 
+application {
+    mainClass.set(/* Main class name here */)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val junitVersion = "5.10.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging { events(TestLogEvent.values()) }
+    testLogging.showStandardStreams = true
+}
