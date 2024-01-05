27f57925692f64d9b7c15c12a0c46fd195b95784
Francesco Bittasi <francesco.bittasi@studio.unibo.it>
2024-01-05 10:48:42 +0100

    created engine mainLoop

--- src/main/java/oop23/gfight/engine/EngineImpl.java
+++ src/main/java/oop23/gfight/engine/EngineImpl.java
@@ -1,6 +1,9 @@
 package oop23.gfight.engine;
 
 public class EngineImpl implements Engine{
+    
+    private static final int frameRate = 60;
+    private final long frameLenght = 1000/frameRate;
 
     @Override
     public void initialize() {
@@ -9,7 +12,42 @@ public class EngineImpl implements Engine{
 
     @Override
     public void mainLoop() {
-        System.out.println("Hello world");
+        long prevFrameStartTime = System.currentTimeMillis();
+        while (isAppRunning()) {
+            long frameStartTime = System.currentTimeMillis();
+			long deltaTime = frameStartTime - prevFrameStartTime;
+            processInput();
+            update(deltaTime);
+            render();
+            waitNextFrame(frameStartTime);
+            prevFrameStartTime = frameStartTime;
+        }
+        System.out.println("App closed");
+    }
+
+    private void waitNextFrame(long frameStartTime) {
+        long dt = System.currentTimeMillis() - frameStartTime;
+		if (dt < frameLenght){
+			try {
+				Thread.sleep(frameLenght - dt);
+			} catch (Exception ex){}
+		}
+    }
+
+    private void render() {
+        System.out.println(counter+"");
+    }
+
+    private int counter=0;
+    private void update(long deltaTime) {
+        counter++;
+    }
+
+    private void processInput() {
+    }
+
+    private boolean isAppRunning() {
+        return true;
     }
 
 }
