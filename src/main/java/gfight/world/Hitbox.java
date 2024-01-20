8ae4925ee3dc77f795b2cd334e61661d11fd3ef7
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2024-01-20 14:29:57 +0100

    A very simple facade that creates hitboxes and test the collision beetween 2 elements

--- src/main/java/gfight/world/Hitbox.java
+++ src/main/java/gfight/world/Hitbox.java
@@ -1,7 +1,34 @@
 package gfight.world;
 
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.prep.PreparedPolygon;
 
 public class Hitbox {
-    
+
+    /**
+     * Creates the polygon of the hitbox of the elements in the game
+     * @param vertexes of the polygon
+     * @return polygon geometry itself
+     */
+    public Geometry getGeometry(Coordinate[] vertexes){
+        final GeometryFactory factory = new GeometryFactory();
+        return factory.createPolygon(vertexes);
+    }
+
+
+    /**
+     * A class that calulates if a Polygon is colliding with another object 
+     * with hitbox
+     * @param collider is the object that can have a change in his behaviour 
+     * or cause that after a collision
+     * @param coollided is the object that can be hitted
+     * @return if the collision happens
+     */
+    boolean isColliding(Polygon collider, Geometry coollided){
+        PreparedPolygon myObject = new PreparedPolygon(collider);
+        return myObject.crosses(coollided);
+    }
 }
