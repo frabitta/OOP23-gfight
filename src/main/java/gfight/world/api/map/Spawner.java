896c034908c2c064bf67cca2d55c9306b62fb7e1
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-01-29 15:43:36 +0100

    Better doc

--- src/main/java/gfight/world/api/map/Spawner.java
+++ src/main/java/gfight/world/api/map/Spawner.java
@@ -5,12 +5,12 @@ import java.util.Set;
 import gfight.world.api.GameEntity;
 
 /**
- * A spawner which spawns enemies on the map
+ * A spawner which spawns enemies on the map.
  */
 public interface Spawner {
 
     /**
-     * Spawn enemies on the map. The spawner will spawn enemies only if it's enabled
+     * Spawn enemies on the map. The spawner will spawn enemies only if it's enabled.
      * 
      * @return a set containing the enemies just spawned,
      *         empty if the spawner is disabled
@@ -18,19 +18,19 @@ public interface Spawner {
     Set<GameEntity> spawnEnemies();
 
     /**
-     * Check whether the spawner is enabled or not
+     * Check whether the spawner is enabled or not.
      * 
      * @return a boolean describing if the spawner is enabled or disabled
      */
     boolean isEnabled();
 
     /**
-     * Enable the spawner
+     * Enable the spawner.
      */
     void enable();
 
     /**
-     * Disable the spawner
+     * Disable the spawner.
      */
     void disable();
 }
