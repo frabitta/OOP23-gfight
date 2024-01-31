a8b22db0cdf23b70a202a8a54c406ed0b467975e
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2024-01-31 15:39:50 +0100

    Fixed Javadoc in GeomOperator

--- src/main/java/gfight/common/impl/GeomOperatorImpl.java
+++ src/main/java/gfight/common/impl/GeomOperatorImpl.java
@@ -5,12 +5,17 @@ import org.locationtech.jts.geom.Coordinate;
 
 import gfight.common.api.GeomOperator;
 
-public class GeomOperatorImpl implements GeomOperator {
-    public Coordinate sum(Coordinate start, Vector2D vector) {
+/**
+ * Implementation of the GeomOperator Interface.
+ */
+public final class GeomOperatorImpl implements GeomOperator {
+    @Override
+    public Coordinate sum(final Coordinate start, final Vector2D vector) {
         return new Coordinate(start.x + vector.getX(), start.y + vector.getY());
     }
 
-    public Vector2D distance(Coordinate point1, Coordinate point2) {
+    @Override
+    public Vector2D distance(final Coordinate point1, final Coordinate point2) {
         return new Vector2D(point1.x - point2.x, point1.y - point2.y);
     }
 }
