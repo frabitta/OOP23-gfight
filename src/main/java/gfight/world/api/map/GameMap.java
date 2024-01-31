af4c0964cd6c0eadfa15e84609a4e4061a1b993f
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-01-31 16:17:29 +0100

    Remove things the GameMap shouldn't do

--- src/main/java/gfight/world/api/map/GameMap.java
+++ src/main/java/gfight/world/api/map/GameMap.java
@@ -5,26 +5,10 @@ import java.util.Set;
 
 import org.locationtech.jts.geom.Coordinate;
 
-import gfight.world.api.GameEntity;
-
 /**
  * The map where the game takes place.
  */
 public interface GameMap {
-
-    /**
-     * Get all the enemies on the map.
-     * 
-     * @return a set containing the enemies on the map
-     */
-    Set<GameEntity> getEnemies();
-
-    /**
-     * Get the player on the map.
-     * 
-     * @return the player entity
-     */
-    GameEntity getPlayer();
 
     /**
      * Get all the game tiles of which the map is composed.
