d96f0135bbde62c5cdf27bbf402a7f34c2dda4f3
Luca Marchi <luca.marchi13@studio.unibo.it>
2024-02-02 18:17:36 +0100

    first implementation of entityFactory

--- src/main/java/gfight/world/api/EntityFactory.java
+++ src/main/java/gfight/world/api/EntityFactory.java
@@ -1,51 +1,53 @@
 package gfight.world.api;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.locationtech.jts.geom.Coordinate;
 
 import gfight.engine.graphics.api.GraphicsComponent;
 import gfight.world.impl.Enemy;
 import gfight.world.impl.Player;
-import gfight.world.movement.api.Movement;
 import gfight.world.weapon.api.Projectile;
 
 public interface EntityFactory {
-    /**
-     * 
-     * @param vertexes
-     * @param position
-     * @param graphicsComponent
-     * @param movement
-     * @param health
-     * @return a new Player
-     */
-    Player createPlayer(List<Coordinate> vertexes, Coordinate position, GraphicsComponent graphicsComponent,
-            Optional<Movement> movement, int health);
-    /**
-     * 
-     * @param vertexes
-     * @param position
-     * @param graphicsComponent
-     * @return a new Obstacle
-     */
-    CachedGameEntity createObstacle(List<Coordinate> vertexes, Coordinate position, GraphicsComponent graphicsComponent);
 
-    /**
-     * 
-     * @param vertexes
-     * @param position
-     * @param graphicsComponent
-     * @param movement
-     * @param health
-     * @return a new Enemy
-     */
-    Enemy createEnemy(List<Coordinate> vertexes, Coordinate position, GraphicsComponent graphicsComponent,
-            Optional<Movement> movement, int health);
-    /**
-     * 
-     * @return a new Projectile
-     */
-    Projectile createProjectile();
+        /**
+         * 
+         * @param sideLength        of the figure
+         * @param position          the center of the player
+         * @param graphicsComponent color of the player
+         * @param health            life point of the player
+         * @return a new Player
+         */
+        Player createPlayer(double sideLength, Coordinate position, GraphicsComponent graphicsComponent, int health);
+
+        /**
+         * 
+         * @param sideLength        of the figure
+         * @param position          the center of the enemy
+         * @param graphicsComponent color of the enemy
+         * @param health            life point of the enemy
+         * @return a new Enemy
+         */
+        Enemy createEnemy(double sideLength, Coordinate position, GraphicsComponent graphicsComponent, int health);
+
+        /**
+         * 
+         * @param sideLength        of the figure
+         * @param position          the center of the obstacle
+         * @param graphicsComponent color of the obstacle
+         * @return a new Obstacle
+         */
+        CachedGameEntity createObstacle(double sideLength, Coordinate position, GraphicsComponent graphicsComponent);
+
+        /**
+         * 
+         * @param sideLength        of the figure
+         * @param position          the centre of the chest
+         * @param graphicsComponent the color of the chest
+         * @param health            life point of the chest
+         * @return
+         */
+        ActiveEntity createChest(double sideLength, Coordinate position, GraphicsComponent graphicsComponent,
+                        int health);
+
+        Projectile createProjectile();
 }
--- src/main/java/gfight/world/impl/EntityFactoryImpl.java
+++ src/main/java/gfight/world/impl/EntityFactoryImpl.java
@@ -1,43 +1,70 @@
 package gfight.world.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.locationtech.jts.geom.Coordinate;
 
 import gfight.engine.graphics.api.GraphicsComponent;
+import gfight.world.api.ActiveEntity;
 import gfight.world.api.CachedGameEntity;
 import gfight.world.api.EntityFactory;
 import gfight.world.movement.api.Movement;
+import gfight.world.movement.impl.BaseMovement;
+import gfight.world.movement.impl.InputMovementImpl;
 import gfight.world.weapon.api.Projectile;
 
 public class EntityFactoryImpl implements EntityFactory {
 
     @Override
-    public Player createPlayer(List<Coordinate> vertexes, Coordinate position, GraphicsComponent graphicsComponent,
-            Optional<Movement> movement, int health) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createPlayer'");
+    public Player createPlayer(double sideLength, Coordinate position, GraphicsComponent graphicsComponent,
+            int health) {
+        final Optional<Movement> movement = Optional.ofNullable(new InputMovementImpl());
+        final List<Coordinate> vertexes = calculatePosition(sideLength, position);
+        final Player player = new Player(vertexes, position, graphicsComponent, movement, health);
+        return player;
     }
 
     @Override
-    public CachedGameEntity createObstacle(List<Coordinate> vertexes, Coordinate position,
+    public Enemy createEnemy(double sideLength, Coordinate position, GraphicsComponent graphicsComponent, int health) {
+        final Optional<Movement> movement = Optional.ofNullable(new InputMovementImpl());
+        final List<Coordinate> vertexes = calculatePosition(sideLength, position);
+        final Enemy enemy = new Enemy(vertexes, position, graphicsComponent, movement, health);
+        return enemy;
+    }
+
+    @Override
+    public CachedGameEntity createObstacle(double sideLength, Coordinate position,
             GraphicsComponent graphicsComponent) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'createObstacle'");
     }
 
     @Override
-    public Enemy createEnemy(List<Coordinate> vertexes, Coordinate position, GraphicsComponent graphicsComponent,
-            Optional<Movement> movement, int health) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createEnemy'");
-    }
-
-    @Override
     public Projectile createProjectile() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'createProjectile'");
+    }
+
+    @Override
+    public ActiveEntity createChest(double sideLength, Coordinate position, GraphicsComponent graphicsComponent,
+            int health) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createChest'");
+    }
+
+    private List<Coordinate> calculatePosition(double sideLength, Coordinate position) {
+        Coordinate firstPoint = new Coordinate(position.x + sideLength / 2,
+                position.y + (sideLength * Math.sqrt(3)) / 2);
+        Coordinate secondPoint = new Coordinate(position.x - sideLength / 2,
+                position.y + sideLength * Math.sqrt(3) / 2);
+        Coordinate thirdPoint = new Coordinate(position.x, position.y - sideLength * Math.sqrt(3) / 2);
+        List<Coordinate> vertexes = new ArrayList<>();
+        vertexes.add(firstPoint);
+        vertexes.add(secondPoint);
+        vertexes.add(thirdPoint);
+        return vertexes;
     }
 
     // crea prima movimento
