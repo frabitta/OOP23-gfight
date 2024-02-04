ae00677ffeb11e0be420440f13c5115249955648
Francesco Bittasi <francesco.bittasi@studio.unibo.it>
2024-02-04 12:07:36 +0100

    Moved Position2D in common.api

---
+++ src/main/java/gfight/common/api/Position2D.java
@@ -0,0 +1,29 @@
+package gfight.common;
+
+import gfight.common.api.Vect;
+
+/**
+ * Interface to store 2D positions.
+ */
+public interface Position2D {
+
+    /**
+     * @return x
+     */
+    double getX();
+
+    /**
+     * @return y
+     */
+    double getY();
+
+    /**
+     * Adds the vector to the current position and returns a new
+     * Position2DImpl representing the result.
+     *
+     * @param a The vector to add.
+     * @return A new Position2DImpl representing the sum of the current position and
+     *         vector.
+     */
+    Position2D sum(Vect a);
+}
