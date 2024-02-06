d5719e5e7851d6d9f2b6caedba97a0ddab71f90d
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2024-02-06 11:00:12 +0100

    fixed a problem in angle calc method with the first condition

--- src/main/java/gfight/common/impl/VectorImpl.java
+++ src/main/java/gfight/common/impl/VectorImpl.java
@@ -86,7 +86,7 @@ public final class VectorImpl extends Vector2D implements Vect {
 
     @Override
     public double anglecalc(Vect v1) {
-        if (v1.getX() != 0 || v1.getY() != 0 && this.getX() != 0 || this.getY() != 0) {
+        if ((v1.getX() != 0 || v1.getY() != 0) && (this.getX() != 0 || this.getY() != 0)) {
             if (v1 instanceof VectorImpl) {
                 return Vector2D.angle(this, (VectorImpl) v1);
             } else {
