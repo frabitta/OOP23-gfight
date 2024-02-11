4271e70ea07bb75d3a9724763aaf1cb41e62eef9
Luca Marchi <luca.marchi13@studio.unibo.it>
2024-02-11 18:29:24 +0100

    Changed bfsMovement for different types of enemies

--- src/main/java/gfight/world/movement/api/MovementFactory.java
+++ src/main/java/gfight/world/movement/api/MovementFactory.java
@@ -1,8 +1,8 @@
 package gfight.world.movement.api;
 
 import gfight.common.api.Vect;
+import gfight.world.entity.api.Character;
 import gfight.world.entity.api.GameEntity;
-import gfight.world.entity.api.MovingEntity;
 import gfight.world.map.api.GameMap;
 import gfight.world.movement.impl.BfsMovement;
 import gfight.world.movement.impl.LinearMovement;
@@ -23,7 +23,7 @@ public interface MovementFactory {
      * @param speed  the speed of the agent
      * @return the BFS based movement object
      */
-    BfsMovement createBfsMovement(GameEntity target, MovingEntity agent, GameMap map, double speed);
+    BfsMovement createBfsMovement(GameEntity target, Character agent, GameMap map, double speed);
 
     /**
      * Creates Linear movement.
--- src/main/java/gfight/world/movement/impl/BfsMovement.java
+++ src/main/java/gfight/world/movement/impl/BfsMovement.java
@@ -5,7 +5,7 @@ import gfight.common.api.Vect;
 import gfight.common.impl.VectorImpl;
 import gfight.world.entity.api.Character;
 import gfight.world.entity.api.GameEntity;
-import gfight.world.entity.api.MovingEntity;
+import gfight.world.entity.api.Character.CharacterType;
 import gfight.world.map.api.GameMap;
 import gfight.world.map.api.GameTile;
 
@@ -22,7 +22,7 @@ import org.jgrapht.alg.shortestpath.BFSShortestPath;
  */
 public final class BfsMovement extends BaseMovement {
     private final GameEntity target;
-    private final MovingEntity agent;
+    private final Character agent;
     private final GameMap map;
     private final double speed;
 
@@ -34,7 +34,7 @@ public final class BfsMovement extends BaseMovement {
      * @param map    of the game
      * @param speed  of the agent
      */
-    public BfsMovement(final MovingEntity agent, final GameEntity target, final GameMap map, final double speed) {
+    public BfsMovement(final Character agent, final GameEntity target, final GameMap map, final double speed) {
         this.target = target;
         this.agent = agent;
         this.map = map;
@@ -43,16 +43,32 @@ public final class BfsMovement extends BaseMovement {
 
     @Override
     public void update() {
-        if (this.agent instanceof Character) {
-            ((Character) this.agent).pointTo(this.target.getPosition());
-        }
+        this.agent.pointTo(this.target.getPosition());
         List<Position2D> path = getPathFromBfs();
-        if (!map.searchTile(agent.getPosition()).equals(map.searchTile(target.getPosition()))) {
-            Vect newDirection = new VectorImpl(agent.getPosition(), path.get(1)).norm().scale(speed);
-            setDirection(newDirection);
-        } else {
-            setDirection(new VectorImpl(0, 0));
+        if (agent.getType() == CharacterType.SHOOTER) {
+            if (agent.getPosition().getDistance(target.getPosition()) < 150) {
+                stopAndAttack();
+            } else {
+                move(path);
+            }
         }
+        if (agent.getType() == CharacterType.RUNNER) {
+            if (!map.searchTile(agent.getPosition()).equals(map.searchTile(target.getPosition()))) {
+                move(path);
+            } else {
+                stopAndAttack();
+            }
+        }
+    }
+
+    private void stopAndAttack() {
+        agent.setDirection(new VectorImpl(0, 0));
+        agent.makeDamage();
+    }
+
+    private void move(List<Position2D> path) {
+        Vect newDirection = new VectorImpl(agent.getPosition(), path.get(1)).norm().scale(speed);
+        setDirection(newDirection);
     }
 
     /**
