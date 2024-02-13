f1561eb35829a09032e32d442b8ae216f0c8eb8a
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2024-02-13 17:14:11 +0100

    Changed Pause panel

--- src/main/java/gfight/view/impl/PausePanel.java
+++ src/main/java/gfight/view/impl/PausePanel.java
@@ -1,36 +1,55 @@
 package gfight.view.impl;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
 import gfight.engine.api.Engine;
 import gfight.engine.api.Engine.EngineStatus;
 
+import java.awt.Image;
+import java.awt.Font;
+import java.awt.Component;
+import java.awt.Graphics;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.Box;
+
 public class PausePanel extends JPanel {
-    private static final long serialVersionUID = -3569782901557207072L;
 
-    PausePanel(final Engine engine) {
-        final JButton continueBTN = new JButton();
-        continueBTN.setText("continue");
-        continueBTN.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.changeStatus(EngineStatus.GAME);
-            }
-        });
-        final JButton gotomenuBTN = new JButton();
-        gotomenuBTN.setText("go to the menu");
-        gotomenuBTN.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.changeStatus(EngineStatus.MENU);
-            }
-        });
+    private final static String PATH_STRING = "src/main/resources/images/";
+    private final Image backgroundImage;
 
-        this.add(continueBTN);
-        this.add(gotomenuBTN);
+    public PausePanel(final Engine engine) {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        backgroundImage = new ImageIcon(PATH_STRING + "Pause.png").getImage();
+
+        final JLabel titleLabel = new JLabel("Paused");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 55));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        final JButton continueButton = new JButton("  Continue   ");
+        continueButton.setFont(new Font("Arial", Font.ITALIC, 22));
+        continueButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        continueButton.addActionListener(e -> engine.changeStatus(EngineStatus.GAME));
+
+        final JButton goToMenuButton = new JButton("Go to Menu");
+        goToMenuButton.setFont(new Font("Arial", Font.ITALIC, 22));
+        goToMenuButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        goToMenuButton.addActionListener(e -> engine.changeStatus(EngineStatus.MENU));
+
+        this.add(Box.createVerticalGlue());
+        this.add(titleLabel);
+        this.add(Box.createVerticalStrut(5));
+        this.add(continueButton);
+        this.add(Box.createVerticalStrut(5));
+        this.add(goToMenuButton);
+        this.add(Box.createVerticalGlue());
+
+    }
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
