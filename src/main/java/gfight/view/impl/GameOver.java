8bbb10db1023283b082dcae69c0b3c2048b0424c
Luca Marchi <luca.marchi13@studio.unibo.it>
2024-02-14 11:29:13 +0100

    changed game over panel

--- src/main/java/gfight/view/impl/GameOver.java
+++ src/main/java/gfight/view/impl/GameOver.java
@@ -1,14 +1,16 @@
 package gfight.view.impl;
 
 import java.awt.Font;
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Component;
 
-import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 import gfight.engine.api.Engine;
 import gfight.engine.api.Engine.EngineStatus;
@@ -19,24 +21,41 @@ import gfight.engine.api.Engine.EngineStatus;
 public class GameOver extends JPanel {
     private static final long serialVersionUID = 4003161718422635256L;
 
-    private static final int TITLE_SIZE = 100;
+    private static final String PATH_STRING = "src/main/resources/images/";
+    private static final int TITLE_FONT_DIM = 70;
+    private static final int BUTTON_FONT_DIM = 22;
+    private static final int TILE_SPACING = 5;
 
+    private final Image backgroundImage;
+
+    /**
+     * Creates a GameOver view.
+     * 
+     * @param engine the game engine
+     */
     public GameOver(final Engine engine) {
-        this.setLayout(new BorderLayout());
-        JLabel title = new JLabel("GAME OVER!!");
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setBorder(BorderFactory.createEmptyBorder(TITLE_SIZE / 4, 0, TITLE_SIZE, 0));
-        title.setFont(new Font("Georgia", Font.BOLD, TITLE_SIZE));
-        title.setForeground(Color.RED);
-        this.setBackground(Color.WHITE);
-        JPanel buttonContainer = new JPanel();
-        buttonContainer.setBackground(Color.WHITE);
-        JButton button = new JButton("Back to menu...");
-        button.setFont(new Font("Arial", Font.BOLD, TITLE_SIZE / 4));
-        button.setForeground(Color.BLUE);
-        button.addActionListener(al -> engine.changeStatus(EngineStatus.MENU));
-        buttonContainer.add(button);
-        add(title, BorderLayout.NORTH);
-        add(buttonContainer, BorderLayout.CENTER);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        backgroundImage = new ImageIcon(PATH_STRING + "game_over2.png").getImage();
+
+        final JLabel titleLabel = new JLabel("GAME OVER!");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, TITLE_FONT_DIM));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        final JButton backToMenu = new JButton("Go to Menu");
+        backToMenu.setFont(new Font("Arial", Font.ITALIC, BUTTON_FONT_DIM));
+        backToMenu.setAlignmentX(Component.CENTER_ALIGNMENT);
+        backToMenu.addActionListener(e -> engine.changeStatus(EngineStatus.MENU));
+
+        this.add(Box.createVerticalGlue());
+        this.add(titleLabel);
+        this.add(Box.createVerticalStrut(TILE_SPACING));
+        this.add(backToMenu);
+        this.add(Box.createVerticalGlue());
+    }
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
