f17513e7a53a7c36d8886d2c8be02049f8cb2207
Luca Marchi <luca.marchi13@studio.unibo.it>
2024-02-16 11:40:01 +0100

    Implemented tests for BfsMovement

--- src/main/java/gfight/world/movement/impl/BfsMovement.java
+++ src/main/java/gfight/world/movement/impl/BfsMovement.java
@@ -68,7 +68,7 @@ public final class BfsMovement extends BaseMovement {
     }
 
     private void handleRunnerBehavior(final List<Position2D> path) {
-        if (agent.getPosition().getDistance(target.getPosition()) < RANGE_RUNNER) {
+        if (agent.getPosition().getDistance(target.getPosition()) <= RANGE_RUNNER) {
             stopAndAttack();
         } else {
             move(path);
